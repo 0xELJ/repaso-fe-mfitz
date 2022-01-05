66abf4e95d67319e09c4a6e3c2ec8ce8741d9e83
Enrique López Jiménez <loj.enrique@gmail.com>
2022-01-04 18:59:15 -0600

    añade primer ejercicio de desestructuración de arreglos

---
+++ desestructuacion-arreglos/desestructuración-arreglos.js
@@ -0,0 +1,15 @@
+const nombres = ["Juan", "Carol", "Enrique", "Martha"];
+
+// Desestructuración de arreglos
+// Es una caracteristica de JS que permite extraer elementos de un arreglo
+// y asignarlo a variables
+
+// const [primerNombre, segundoNombre, tercerNombre, cuartoNombre, quintoNombre = "Sin nombre"] = nombres;
+console.log("Desestructuración...");
+// console.log("Nombres: ", primerNombre, segundoNombre, tercerNombre, cuartoNombre, quintoNombre);
+
+// const [,,tercerNombre, cuartoNombre] = nombres;
+// console.log("Nombres: ", tercerNombre, cuartoNombre);
+
+const [nombre1,,,nombre4] = nombres;
+console.log("Nombres: ", nombre1, nombre4);
